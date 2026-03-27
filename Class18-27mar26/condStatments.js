8aafe1763bc6b89c10f54bec92c5b657d54e3aeb
Farhan <farhan20574@outlook.com>
2026-03-27 15:34:59 +0000

    class end

---
+++ Class18-27mar26/condStatments.js
@@ -0,0 +1,11 @@
+let num = parseInt (prompt(`Enter your number`))
+
+
+
+if (num % 2 == 0 ) {
+    document.write(`Your number (${num}) is a Event number`)
+}
+else{
+    document.write(`Your number (${num} is a Odd number)`)
+}
+
