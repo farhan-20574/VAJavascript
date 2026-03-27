f2428523475ff6f97ba9146fe6b6842f58164222
Farhan <farhan20574@outlook.com>
2026-03-27 15:01:06 +0000

    calculator

---
+++ Class18-27mar26/promo.js
@@ -0,0 +1,7 @@
+let num1 =parseInt (prompt ("Enter a 1st number "))
+let num2 =parseInt( prompt ("Enter a 2nd number "))
+
+document.write(`Addition: ${num1+num2} <br>`)
+document.write(`Subtraction: ${num1-num2} <br> `)
+document.write(`Multiplication: ${num1*num2}<br> `)
+document.write(`Division: ${num1/num2} <br>`)
