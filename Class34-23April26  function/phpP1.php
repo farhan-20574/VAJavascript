2cd08fca5e30d4e969b7f1c7662cda7b93574acb
Farhan <farhan20574@outlook.com>
2026-04-23 15:23:32 +0000

    class middle

---
+++ Class34-23April26  function/phpP1.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP form</title>
+</head>
+
+<body>
+    <h2>Project</h2>
+    <form action="" method="POST">
+
+        number 1 : <input type="number" name="num1"><br>
+        number 2 : <input type="number" name="num2"><br><br>
+        marks : <input type="number" name="marks"><br><br>
+        <input type="submit" name="add" value="Add">
+        <input type="submit" name="percent" value="Percentage">
+        <input type="submit" name="grade" value="Grade">
+
+    </form>
+
+    <?php
+    function add($a, $b)
+    {
+        return $a  + $b;
+    }
+    function percentage($marks)
+    {
+
+        return $marks;
+    }
+
+    function grade($marks)
+    {
+        if ($marks >= 80) {
+            return "A";
+        } elseif ($marks >= 60) {
+            return "B";
+        } else {
+            return "Fail";
+        }
+    }
+    if ($_POST){
+        if(isset($_POST['add'])){
+            
+        }
+    }
+    ?>
+</body>
+
+</html>
